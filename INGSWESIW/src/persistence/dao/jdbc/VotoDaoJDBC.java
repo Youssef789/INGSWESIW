5d29505ceef7d4fef2460b1f6c9bcd07b160c8bb
R0ckY909 <manuelinos.projects@gmail.com>
2018-01-25 17:51:51 +0100

    Change-Id: Ia2b3fb77733127f364dad4a0fe700ae27e748cd5 Signed-off-by: R0ckY909 <manuelinos.projects@gmail.com>

--- INGSWESIW/src/persistence/dao/jdbc/VotoDaoJDBC.java
+++ INGSWESIW/src/persistence/dao/jdbc/VotoDaoJDBC.java
@@ -2,14 +2,17 @@ package persistence.dao.jdbc;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
 import model.Utente;
 import model.Voto;
 import persistence.DataSource;
+import persistence.DipartimentoDaoJDBC;
 import persistence.IdBroker;
 import persistence.PersistenceException;
+import persistence.dao.RicettaDao;
 import persistence.dao.VotoDao;
 
 public class VotoDaoJDBC implements VotoDao {
@@ -26,35 +29,58 @@ public class VotoDaoJDBC implements VotoDao {
 		try {
 			Long id = IdBroker.getId(connection);
 			voto.setId(id);
-			String insert = "insert into voto (id, valore, ricetta_id, utente_id) values (?, ?, ?, ?)";
+			String insert = "insert into voto (id, valore, ricetta_id, utente_username) values (?, ?, ?, ?)";
 			PreparedStatement statement = connection.prepareStatement(insert);
 			statement.setLong(1, voto.getId());
 			statement.setLong(2, voto.getValore());
-			statement.setLong(3,voto.getRicetta().getId());
-			statement.setString(4,voto.getUtente().getUsername());
+			statement.setLong(3, voto.getRicetta().getId());
+			statement.setString(4, voto.getUtente().getUsername());
 			statement.executeUpdate();
-		}catch (SQLException  e) {
+		} catch (SQLException e1) {
 			if (connection != null) {
 				try {
 					connection.rollback();
-				} catch(SQLException excep) {
-					throw new PersistenceException(e.getMessage());
+				} catch(SQLException e2) {
+					throw new PersistenceException(e2.getMessage());
 				}
 			} 
-		}finally {
+		} finally {
 			try {
 				connection.close();
 			} catch (SQLException e) {
 				throw new PersistenceException(e.getMessage());
 			}
 		}
-
 	}
 
 	@Override
 	public Voto findByPrimaryKey(Long id) {
-		// TODO Auto-generated method stub
-		return null;
+		Connection connection = this.dataSource.getConnection();
+		Voto voto = null;
+		try {
+			PreparedStatement statement;
+			String query = "select * from voto where id = ?";
+			statement = connection.prepareStatement(query);
+			statement.setLong(1, id);
+			ResultSet result = statement.executeQuery();
+			if (result.next()) {
+				voto = new Voto();
+				voto.setId(result.getLong("id"));
+				voto.setValore(result.getInt("valore"));
+				RicettaDao ricettaDao = new RicettaDaoJDBC(dataSource);
+				Ricetta ricetta = 
+				voto.setPassword(result.getString("password"));	
+			}
+		} catch (SQLException e) {
+			throw new PersistenceException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new PersistenceException(e.getMessage());
+			}
+		}	
+		return utente;
 	}
 
 	@Override
@@ -65,14 +91,41 @@ public class VotoDaoJDBC implements VotoDao {
 
 	@Override
 	public void update(Voto voto) {
-		// TODO Auto-generated method stub
-
+		Connection connection = this.dataSource.getConnection();
+		try {
+			String update = "update voto set valore = ?, ricetta_id = ?, utente_username = ? where id = ?";
+			PreparedStatement statement = connection.prepareStatement(update);
+			statement.setInt(1, voto.getValore());
+			statement.setLong(2, voto.getRicetta().getId());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new PersistenceException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new PersistenceException(e.getMessage());
+			}
+		}
 	}
 
 	@Override
 	public void delete(Voto voto) {
-		// TODO Auto-generated method stub
-
+		Connection connection = this.dataSource.getConnection();
+		try {
+			String delete = "delete from voto where id = ?";
+			PreparedStatement statement = connection.prepareStatement(delete);
+			statement.setLong(1, voto.getId());
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			throw new PersistenceException(e.getMessage());
+		} finally {
+			try {
+				connection.close();
+			} catch (SQLException e) {
+				throw new PersistenceException(e.getMessage());
+			}
+		}
 	}
 
 }
